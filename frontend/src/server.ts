415d9113bd1343ee2ec93eacf6250517d7738747
Lung Andreea Selena <lungandreeaselena@gmail.com>
2025-06-14 05:12:22 +0300

    update

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -32,10 +32,6 @@ app.use('/**', (req, res, next) => {
     .catch(next);
 });
 
-/**
- * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, () => {
@@ -43,7 +39,4 @@ if (isMainModule(import.meta.url)) {
   });
 }
 
-/**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
- */
 export const reqHandler = createNodeRequestHandler(app);
